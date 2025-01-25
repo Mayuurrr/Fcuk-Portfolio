662aaf4340d67170226c973e5ac4b5f2992782f8
Mayur Hegde <117928527+Mayuurrr@users.noreply.github.com>
2025-01-25 19:54:59 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Mayuurrr",
     resume:
-      "https://drive.google.com/file/d/1jeNPobLoxpwiJ-yIcCS8pxHcZHC3Gxu4/view?usp=sharing",
+      "https://drive.google.com/file/d/1Mwgs6iNsVRDspotFsdpJehPUW_JTbMT1/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in",
     twitter: "https://twitter.com",
     insta: "https://www.instagram.com",
